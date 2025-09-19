5b31f638ca7df351b5687143af4f3c4688ecf418
Hinisha Bansal <84309569+hbansal-eightfold@users.noreply.github.com>
2025-09-19 17:05:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
+from flask import Flask, render_template
+from gym_system import GymSystem
 
+app = Flask(__name__)
+gym = GymSystem()
+
+# Seed some data
+gym.log_workout("Ananya", 800)
+gym.log_workout("Rahul", 700)
+gym.log_workout("Priya", 600)
+gym.log_workout("Ananya", 700)
+gym.log_workout("Priya", 900)
+gym.log_workout("Rahul", 800)
+
+@app.route("/")
+def leaderboard():
+    leaderboard = gym.get_leaderboard()
+    return render_template("leaderboard.html", leaderboard=leaderboard)
+
+if __name__ == "__main__":
+    app.run(debug=True)
