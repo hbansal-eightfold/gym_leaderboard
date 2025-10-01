b9054439d4f712c86b32b4db22fe5570bcbd2a1e
Hinisha Bansal <84309569+hbansal-eightfold@users.noreply.github.com>
2025-10-01 09:46:55 +0530

    Update member_stats.py

--- models/member_stats.py
+++ models/member_stats.py
@@ -1,1 +1,9 @@
+class MemberStats:
+    def __init__(self, name):
+        self.name = name
+        self.workouts = 0
+        self.calories = 0
 
+    def log_workout(self, calories):
+        self.workouts += 1
+        self.calories += calories
